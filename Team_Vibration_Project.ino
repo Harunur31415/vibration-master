f1b69dae706e6cb7759786d821ea0fe6998ff0b8
moonfarhan <55107318+moonfarhan@users.noreply.github.com>
2019-09-10 11:38:17 +0600

    Vibration master code

---
+++ Team_Vibration_Project.ino
@@ -0,0 +1,20 @@
+
+int vs =9; // vibration sensor
+
+void setup(){
+  pinMode(vs, INPUT); 
+  Serial.begin(9600); 
+
+}
+void loop(){
+  long measurement =vibration();
+  delay(50);
+  Serial.print(measurement);
+  Serial.print(";");
+  
+}
+
+long vibration(){
+  long measurement=pulseIn (vs, HIGH);  //wait for the pin to get HIGH and returns measurement
+  return measurement;
+}
